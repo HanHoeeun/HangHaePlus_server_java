58b3efc31834279d9536b18875accd28c65b20d0
hoeeunhan <heu09277@gmail.com>
2025-09-16 23:48:09 +0900

    설정 추가 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -64,4 +64,8 @@ dependencies {
 
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+
+	implementation("org.flywaydb:flyway-core")
+	implementation("org.flywaydb:flyway-database-postgresql")
+
 }

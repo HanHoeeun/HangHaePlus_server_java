78e1da2453db1a075300ed67250cd85cd9f588fc
hoeeunhan <heu09277@gmail.com>
2025-10-01 00:23:59 +0900

    2) 만료 시간 확인 테스트 만료 이후 다른 유저가 예약 가능해지는지 확인

--- src/main/java/kr/hhplus/be/server/clean/reservation/port/out/TimeProvider.java
+++ src/main/java/kr/hhplus/be/server/clean/reservation/port/out/TimeProvider.java
@@ -1,3 +1,3 @@
-package kr.hhplus.be.server.clean.reservation.port;
+package kr.hhplus.be.server.clean.reservation.port.out;
 import java.time.Instant;
 public interface TimeProvider { Instant now(); }
